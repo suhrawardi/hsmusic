c48e310844f0e0a7fce48aff095f68421ff87361
jarra <suhrawardi@gmail.com>
2017-02-25 23:19:58 +0100

    First Midi thingies

--- Main.hs
+++ Main.hs
@@ -5,14 +5,13 @@
 
 module Main where
 
-actionList = [putStr2 "Music\n",
-              putStr2 "In dah house!\n"]
+import Euterpea
 
-putCharList :: String -> [IO()]
-putCharList = map putChar
-
-putStr2 :: String -> IO()
-putStr2 s = sequence_ (putCharList s)
+t251 :: Music Pitch
+t251 = let dMinor = d 4 wn :=: f 4 wn :=: a 4 wn
+           gMajor = g 4 wn :=: b 4 wn :=: d 5 wn
+           cMajor = c 4 bn :=: e 4 bn :=: g 4 bn
+       in dMinor :+: gMajor :+: cMajor
 
 main :: IO ()
-main = sequence_ actionList
+main = play t251
