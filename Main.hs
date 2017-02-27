c791cf9deca352376868df8b80bf92cce8c4a528
jarra <suhrawardi@gmail.com>
2017-02-27 22:53:33 +0100

    Now we play some notes

--- Main.hs
+++ Main.hs
@@ -7,11 +7,19 @@ module Main where
 
 import Euterpea
 
+concertA, a440 ::(PitchClass,Octave)
+concertA       = (A, 4) -- concert A
+a440           = (A, 4) -- A440
+
 t251 :: Music Pitch
 t251 = let dMinor = d 4 wn :=: f 4 wn :=: a 4 wn
            gMajor = g 4 wn :=: b 4 wn :=: d 5 wn
            cMajor = c 4 bn :=: e 4 bn :=: g 4 bn
        in dMinor :+: gMajor :+: cMajor
 
+wts :: Pitch -> [ Music Pitch ]
+wts p = let f ap = note qn (pitch (absPitch p + ap))
+        in map f [0,2,4,6,8]
+
 main :: IO ()
-main = play t251
+main = play (line (wts a440))
